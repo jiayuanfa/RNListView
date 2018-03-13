50037a12c8ea56fdbe4a97049dccc9a816e66533
贾元发 <jiayuanfa@yeah.net>
2018-03-13 10:31:06 +0800

    初步完成第一个ListView的设计

---
+++ ListViewTest2.js
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  ListView,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+
+type Props = {};
+export default class ListViewTest2 extends Component<Props> {
+  render() {
+    return (
+      <View></View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  
+});
